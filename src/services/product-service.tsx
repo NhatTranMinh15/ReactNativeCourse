ead004f9b0b3f40b6222bd51011b57d0e919457b
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-08-14 17:25:16 +0700

    get product list and product details

---
+++ src/services/product-service.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { ProductModel } from "../models/product";
+import { getValueFor } from "./api-service";
+import { localhost } from "./user-service";
+async function fetcher(url: string) {
+    const token = await getValueFor("token");
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: "Bearer " + token
+        }
+    });
+    return response.data;
+}
+export const getProductUrl = () => localhost + "/product/";
+export const getProductDetailUrl = (productId: string|number) => localhost + "/product/" + productId;
+
+export const ProductFetcher = async (url: string) => {
+    const response = await fetcher(url);
+    const products: ProductModel[] = await response.data;
+    return products;
+}
+
+export const ProductDetailFetcher = async (url: string) => {
+    const response = await fetcher(url);
+    const product: ProductModel = await response.data;
+    return product;
+}
